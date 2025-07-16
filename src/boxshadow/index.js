914255e0c8b1e444a6d3958def4ca4db7f7493d7
alisha <mahjnalisha@gmail.com>
2025-07-17 09:18:19 +1000

    box shadow generator

--- src/boxshadow/index.js
+++ src/boxshadow/index.js
@@ -1,14 +1,37 @@
+const ele = document.getElementById("element");
+const code = document.getElementById("code");
+const sliders = document.querySelectorAll(".slider-wrapper input");
+sliders.forEach((slider) => slider.addEventListener("input", generateBoxShadow))
+function generateBoxShadow() {
+    const shadowParams = getShadowParams();
+    console.log(shadowParams);
+    const boxShadow = createBoxShadow(shadowParams);
+}
 
-function validateInput() {
-    let inputField = document.getElementById("validateInput");
-    let inputFieldVal = inputField.value.trim();
-    const error = document.getElementById("errorMessage");
-    if (inputFieldVal === "") {
-        error.style.visibility = "visible";
-        setTimeout(function () {
-            error.style.visibility = "hidden";
-        }, 5000)
-    } else {
-        alert("Valid input")
-    }
+function getShadowParams() {
+    const hShadow = parseInt(document.getElementById("h-shadow").value);
+    const vShadow = parseInt(document.getElementById("v-shadow").value);
+    const blur = parseInt(document.getElementById("blur-radius").value);
+    const spread = parseInt(document.getElementById("spread-radius").value);
+    const shadowcolor = parseInt(document.getElementById("shadow-color").value);
+    const shadowcoloropacity = parseInt(document.getElementById("shadow-color--opacity").value).toFixed(1);
+
+    const shadowInset = document.getElementById("shadow-inset").checked;
+    return [hShadow, vShadow, blur, spread, shadowcolor, shadowcoloropacity, shadowInset]
+}
+
+function createBoxShadow(hShadow, vShadow, blur, spread, shadowcolor, shadowcoloropacity, shadowInset) {
+    const shadow = shadowInset ? "inset" : "";
+    const rgbaColor = hexToRgba(shadowcolor, shadowcoloropacity);
+    return `${shadow}${hShadow}px ${blur}px ${spread}px ${rgbaColor}px`
+
+
+}
+
+function hexToRgba(shadowcolor, shadowcoloropacity) {
+    console.log(shadowcoloropacity);
+    // const r = parseInt(shadowcolor.substr(1, 2), 16);
+    // const g = parseInt(shadowcolor.substr(3, 2), 16);
+    // const b = parseInt(shadowcolor.substr(5, 2), 16);
+    // return `rgba(${r}${g}${b}, ${shadowcoloropacity})`
 }
